cda56191c2a13b934e6022a11a912db621c1eb36
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-31 14:00:52 +0300

    Create a getter for error message

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,6 +1,16 @@
 public class Decrypt {
-    public Decrypt(String encryptedText, int key){
+    private String decipher="";
+    private String encryptedText;
+    private int decryptKey;
+    private String errorMessage="";
+    public Decrypt(String encryptedText, int decryptKey){
 
     }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+
 
 }
