82f232c220bf9783789fb277cd7a229f33d0624d
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-31 13:59:57 +0300

    Test for error message

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -8,6 +8,11 @@ public class DecryptTest {
         Decrypt testCaesar = new Decrypt("HI",2);
         assertTrue(testCaesar instanceof Decrypt);
     }
+    @Test
+    public void decrypt_testErrorMessage_Integer(){
+        Decrypt testError = new Decrypt("HI",0);
+        assertEquals("",testError.getErrorMessage());
+    }
 
 
 }
