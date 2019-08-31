a7dbb26c84d311a4b72ee34d3f2d7289e3205ae1
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-31 20:03:27 +0300

    Add setters and getters

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,16 +1,28 @@
 public class Decrypt {
     private String decipher="";
     private String encryptedText;
-    private int decryptKey;
+    private int decryptKey=2;
     private String errorMessage="";
     public Decrypt(String encryptedText, int decryptKey){
 
+        if(decryptKey<1 || decryptKey>25){
+            errorMessage= "Key must be between 0 and 25";
+        }
+        else {
+
+        }
     }
 
-    public String getErrorMessage() {
+
+    public String setErrorMessage() {
         return errorMessage;
     }
 
+    public void setEncryptedText(String encryptedText) {
+        this.encryptedText = encryptedText;
+    }
 
-
+    public String getEncryptedText() {
+        return encryptedText;
+    }
 }
