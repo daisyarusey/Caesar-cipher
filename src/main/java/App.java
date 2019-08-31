d49988a3602d997bac02cd731c6ae8b3c39e7234
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-31 20:52:57 +0300

    Prompt user to enter Text and key

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Welcome to  Ceaser Cipher ");
+        System.out.println("Enter text to encrypt");
+        String text = myScanner.nextLine();
+        System.out.println("Enter shift key");
+        int key =myScanner.nextInt();
+
+
+
+
+    }
+
+
+
+}
