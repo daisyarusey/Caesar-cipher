615ce625418ea1ce3dca45843256d3b649cf6db7
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-31 13:45:57 +0300

    Test instantiation

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecryptTest {
+    @Test
+    public void decrypt_instantiatesCorrectly_true() {
+        Decrypt testCaesar = new Decrypt("HI",2);
+        assertTrue(testCaesar instanceof Decrypt);
+    }
+
 
 }
