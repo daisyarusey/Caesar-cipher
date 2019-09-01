9ac2e5a50ee3aa541e7238a262bfc154af741f22
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-01 10:41:03 +0300

    Add if statement to print error message when the key and decryptKey is less than 1 and more than 25

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,26 +10,38 @@ public class App {
         System.out.println("Do you want to encrypt or decrypt?");
         String choice = myScanner.next();
 
+
+
         if (choice.equalsIgnoreCase("encrypt")) {
             System.out.println("Let's now encrypt your text");
             System.out.println("Enter text to encrypt");
             String text = myScanner.next();
             System.out.println("Enter shift key");
             int key = myScanner.nextInt();
+            if (key<1 || key>25){
 
-            Encrypt encrypt = new Encrypt(text,key);
-            String cipher = encrypt.setCipher();
-            System.out.println("Here is your encrypted text:"+" "+cipher);
+                System.out.println("Key must be between 1 and 25");
+            }else {
+
+                Encrypt encrypt = new Encrypt(text, key);
+                String cipher = encrypt.setCipher();
+                System.out.println("Here is your encrypted text:" + " " + cipher);
+            }
 
         } else if (choice.equalsIgnoreCase("decrypt")) {
             System.out.println("Enter encrypted text");
             String encryptedText = myScanner.next();
             System.out.println("Enter the shift key");
             int decryptKey = myScanner.nextInt();
+            if (decryptKey<1 || decryptKey>25){
 
-            Decrypt decrypt = new Decrypt(encryptedText,decryptKey);
-            String decipher = decrypt.setDecipher();
-            System.out.println("Here is your decrypted text:"+" "+ decipher);
+                System.out.println("Key must be between 1 and 25");
+            }else {
+
+                Decrypt decrypt = new Decrypt(encryptedText, decryptKey);
+                String decipher = decrypt.setDecipher();
+                System.out.println("Here is your decrypted text:" + " " + decipher);
+            }
         }else {
             System.out.println("Invalid input");
 
