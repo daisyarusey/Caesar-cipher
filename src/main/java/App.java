44055aa9cdcae517429b2a634242d64d2c13bc9d
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-01 10:23:53 +0300

    Create new instance of class Encrypt and print the cipher to the user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,14 +10,16 @@ public class App {
         System.out.println("Do you want to encrypt or decrypt?");
         String choice = myScanner.next();
 
-        if (choice.equals("encrypt")) {
+        if (choice.equalsIgnoreCase("encrypt")) {
             System.out.println("Let's now encrypt your text");
             System.out.println("Enter text to encrypt");
             String text = myScanner.next();
             System.out.println("Enter shift key");
             int key = myScanner.nextInt();
 
-
+            Encrypt encrypt = new Encrypt(text,key);
+            String cipher = encrypt.setCipher();
+            System.out.println("Here is your encrypted text:"+" "+cipher);
 
         } else if (choice.equalsIgnoreCase("decrypt")) {
             System.out.println("Enter encrypted text");
